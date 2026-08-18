88b62bc80a535cd0d268bbbea948bb559ded4fcc
netmadan <netmadan@gmail.com>
2026-08-18 04:02:24 +0000

    Build to docs folder for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/indraWeek5/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react()],
 })
